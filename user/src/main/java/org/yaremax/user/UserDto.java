72c8d96f952b4e44431f4c52929b3f1eb580feff
Maksym Yarema <macsym2@gmail.com>
2024-02-01 08:33:26 +0200

    remove unused User module

--- user/src/main/java/org/yaremax/user/UserDto.java
+++
@@ -1,4 +0,0 @@
-package org.yaremax.user;
-
-public record UserDto(String username) {
-}
--- user/src/main/java/org/yaremax/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.yaremax.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUsername(String username);
-}

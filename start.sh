d9fa46f30b4c6c79c9dc4720d4e2426c05c6f0dc
Maksym Yarema <macsym2@gmail.com>
2024-02-06 18:05:27 +0200

    fix(start.sh): add delay to wait for db nodes to prepare for init

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 docker-compose up -d crdb-1 crdb-2
 
+sleep 5
+
 docker-compose exec crdb-1 ./cockroach init --insecure
 docker-compose exec crdb-1 ./cockroach sql --insecure --execute="CREATE DATABASE twt_feed_db;"
 
